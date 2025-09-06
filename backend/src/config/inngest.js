f2368bd540f49480cd0f3119733224f82bfbc281
unknown <HiteshB22>
2025-09-06 18:49:19 +0530

    inngest fix

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -11,7 +11,7 @@ const syncUser = inngest.createFunction(
     {event:"clerk/user.created"},
     async ({ event })=> {
         await connectDB();
-        const {id, email_addresses, first_name, last_name} = event.data;
+        const {id, email_addresses, first_name, last_name,image_url} = event.data;
 
         const newUser ={
             clerkId: id,
